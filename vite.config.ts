5cac0feb69b9e391760d7159f400d20bfa4888e4
H1rono <hronok66@gmail.com>
2022-12-13 02:33:24 +0900

    :green_heart: Fix CI error on pushing to gitea

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+    const node_env = process.env.NODE_ENV || "build";
+    const out = node_env === "ci-build" ? ".output" : "dist";
+    const config = {
+        plugins: [react()],
+        build: {
+            outDir: out
+        }
+    };
+    return config;
+});
