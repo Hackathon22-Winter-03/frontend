3abab50058c45079e756cefd20a5a205f9ba279e
H1rono <hronok66@gmail.com>
2022-12-14 00:06:25 +0900

    :sparkles: ページのコンポーネントを追加
    雛形のみ

---
+++ src/pages/Problem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Problem = () => {
+    return <h1>This is the problem page.</h1>;
+};
+
+export default Problem;
---
+++ src/pages/ProblemList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProblemList = () => {
+    return <h1>This is the problem list page.</h1>;
+};
+
+export default ProblemList;
