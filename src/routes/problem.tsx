ab5c2262d84673d1ffe6eda7c0f4ddf0f87947f6
H1rono <hronok66@gmail.com>
2022-12-15 00:54:53 +0900

    :sparkles: Add routing components

---
+++ src/routes/problem.tsx
@@ -0,0 +1,17 @@
+import { default as ProblemPage } from "../pages/Problem";
+
+export async function action() {
+    // TODO: implement for React Router
+    return null;
+}
+
+export async function loader() {
+    // TODO: implement for React Router
+    return null;
+}
+
+const Problem = () => {
+    return <ProblemPage />;
+};
+
+export default Problem;
