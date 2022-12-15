ab619281d84d1c0c79fbd3be429483e06f41c8ef
saitenntaisei <saitenntaisei@gmail.com>
2022-12-16 00:11:33 +0900

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,49 @@
 import { Link } from "react-router-dom";
 function Hackson() {
     return (
-        <div>
-            <Link to="/problems" className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">Hackson</Link>
-        </div>
+        <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900 text-3xl">
+            はっくしょん
+        </Link>
     );
 }
-export default function Header() {
+function Problems() {
     return (
-        <div>
-            <Hackson />
+        <Link to="/problems" className="text-base font-medium text-gray-500 hover:text-gray-900  text-3xl">
+            問題
+        </Link>
+    );
+}
+function Players() {
+    return (
+        <Link to="/ranking" className="text-base font-medium text-gray-500 hover:text-gray-900 text-3xl">
+            プレイヤー
+        </Link>
+    );
+}
+function Home({ playerId }: { playerId: string }) {
+    return (
+        <Link to="/user" className="text-base font-medium text-gray-500 hover:text-gray-900 text-3xl">
+            {playerId}
+        </Link>
+    );
+}
+
+export default function Header({ playerId }: { playerId: string }) {
+    return (
+        <div className="bg-slate-300   flex justify-between items-center text-white text-5xl">
+            <div className="px-4">
+                <Hackson />
+            </div>
+            <div className="px-8">
+                <Problems />
+            </div>
+            <div className="px-16">
+                <Players />
+            </div>
+
+            <div className="px-20">
+                <Home playerId={playerId} />
+            </div>
         </div>
     );
 }
