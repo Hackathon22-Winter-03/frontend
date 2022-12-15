824b9305bdc97ee06581eeed4cb00458070138de
H1rono <hronok66@gmail.com>
2022-12-15 23:50:58 +0900

    :sparkles: Improve routes/root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 export async function action() {
     // TODO: implement for React Router
@@ -11,7 +12,28 @@ export async function loader() {
 }
 
 const Root = () => {
-    return <Outlet />;
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to="/">index</Link>
+                </li>
+                <li>
+                    <Link to="problem">problem</Link>
+                </li>
+                <li>
+                    <Link to="problems">problems</Link>
+                </li>
+                <li>
+                    <Link to="ranking">ranking</Link>
+                </li>
+                <li>
+                    <Link to="user">user</Link>
+                </li>
+            </ul>
+            <Outlet />
+        </>
+    );
 };
 
 export default Root;
