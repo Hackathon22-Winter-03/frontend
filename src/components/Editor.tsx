4ffeb759ba36a51e8ae42f3df6388b9ea4b7d815
saitenntaisei <saitenntaisei@gmail.com>
2022-12-16 20:16:04 +0900

    add gfm

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,19 @@
 import { useRemark } from "react-remark";
 import Editor from "@monaco-editor/react";
+import remarkGfm from 'https://esm.sh/remark-gfm@3';
+import { ReactElement } from "react";
 
-function Preview({ reactContent }) {
-    return <>{reactContent}</>;
+function Preview({ reactContent }:{reactContent:ReactElement|null}) {
+    return <div className='whitespace-pre'>{reactContent}</div>;
 }
 
-export default function MDEditor() {
+export default function MDEditor({value,setValue}) {
     const [reactContent, setMarkdownSource] = useRemark();
     function handleEditorChange(value: string | void) {
         if (value) {
+            console.log(value);
             setMarkdownSource(value);
+            setValue(value)
         }
     }
     return (
