07b7bfddcf7b824c5a274b65734d5eadec4b8c57
H1rono <hronok66@gmail.com>
2022-12-18 04:58:50 +0900

    :lipstick: implement ranking

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,5 +1,36 @@
+import { useLoaderData } from "react-router";
+import { Link } from "react-router-dom";
+import { default as UserModel } from "../models/User";
+
 const Ranking = () => {
-    return <h1>This is the ranking page.</h1>;
+    const users = useLoaderData() as UserModel[];
+    return (
+        <>
+            <h1>ランキング</h1>
+            <table className="border-collapse my-8 min-w-full text-black">
+                <thead className="border-b-2">
+                    <tr className="text-left">
+                        <th className="p-1">順位</th>
+                        <th className="p-1">名前</th>
+                        <th className="p-1">スコア</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user: UserModel, i: number) => (
+                        <tr key={user.id} className="border-t">
+                            <td className="p-1">{i + 1}</td>
+                            <td className="p-1 text-blue-500">
+                                <Link to={`/users/${user.name}`} className="hover:underline">
+                                    {user.name}
+                                </Link>
+                            </td>
+                            <td className="p-1">{user.score}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default Ranking;
