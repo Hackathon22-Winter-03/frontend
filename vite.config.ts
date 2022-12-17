9002c54847714ae3afefe668c6eb093d2d586052
saitenntaisei <saitenntaisei@gmail.com>
2022-12-17 20:32:22 +0900

    add ip?

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -8,8 +8,11 @@ export default defineConfig(() => {
     const config = {
         plugins: [react()],
         build: {
-            outDir: out
-        }
+            outDir: out,
+        },
+        server: {
+            host: true,
+        },
     };
     return config;
 });
