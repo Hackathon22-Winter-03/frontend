0e4ea536cebd64e45e3dbe7e8d17a53fa52c863b
H1rono <hronok66@gmail.com>
2022-12-17 10:13:23 +0900

    :sparkles: Implement problem page
    表のUIのみ

--- src/routes/problem.tsx
+++ src/routes/problem.tsx
@@ -1,13 +1,23 @@
 import { default as ProblemPage } from "../pages/Problem";
+import { default as ProblemModel } from "../models/Problem";
 
 export async function action() {
     // TODO: implement for React Router
     return null;
 }
 
-export async function loader() {
+export async function loader(): Promise<ProblemModel> {
     // TODO: implement for React Router
-    return null;
+    // this data is sample
+    return {
+        id: "0xabcdef",
+        creatorId: "hackson",
+        score: 10,
+        title: "Hello, world",
+        createdAt: 20221216,
+        updatedAt: 20221216,
+        deletedAt: undefined,
+    };
 }
 
 const Problem = () => {
