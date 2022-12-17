bf97e7d068ceebbf4bc239c29cfb896b84cdffae
saitenntaisei <saitenntaisei@gmail.com>
2022-12-18 01:12:19 +0900

    fix-length

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -4,11 +4,11 @@ import Modal from "react-modal";
 const customStyles = {
     content: {
         top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
+        left: "75%",
+        right: "75%",
+        bottom: "30%",
+        marginRight: "-25%",
+        transform: "translate(-150%, -50%)",
     },
 };
 
@@ -30,7 +30,7 @@ function ResultModal({
     return (
         <>
             <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
-                <h1 className="text-red-700">{result}</h1>
+                <h1 className="text-red-700 text-center">{result}</h1>
 
                 <button
                     className="rounded-sm bg-stone-500 text-white p-2 bottom-0 right-0 absolute hover:bg-purple-600"
