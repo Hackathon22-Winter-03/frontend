baeb88ac5a8587bf11e84a3e99ef6f364569d2df
anko9801 <daiusa3@icloud.com>
2022-12-18 12:41:43 +0900

    step execution

--- src/pages/Problem.tsx
+++ src/pages/Problem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useLoaderData } from "react-router";
 import CodeEditor, { CodeEditorProp } from "../components/CodeEditor";
 import Preview from "../components/Preview";
@@ -17,65 +17,38 @@ const Description = ({ title, md }: DescriptionProp) => (
         <Preview value={md} />
     </div>
 );
-function Submission({
-    openModal,
-    setOpenModal,
-    value,
-    setValue,
-}: {
-    openModal: boolean;
-    setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
-    value: string;
-    setValue: React.Dispatch<React.SetStateAction<string>>;
-}) {
-    const [source, setSource] = useState("");
-
-    return (
-        <div className="bg-yellow-200 rounded m-4 p-4 basis-full flex flex-wrap">
-            <h3 className="w-full my-3">提出 / 実行</h3>
-            <input
-                type="text"
-                value={source}
-                onChange={(e) => setSource(e.target.value)}
-                id="source"
-                className="w-full h-8 my-3 font-mono"
-            />
-
-            <CodeEditor value={value} setValue={setValue} />
-        </div>
-    );
-}
 
 const Problem = () => {
     const { problem, userId } = useLoaderData() as { problem: ProblemModel; userId: string };
     const [code, setCode] = useState("");
     const [input, setInput] = useState("");
     const [openModal, setOpenModal] = useState(false);
-    function nextStep() {
+    const inputRef = useRef(input);
+    inputRef.current = input;
+
+    const nextStep = async () => {
         const bodyFormData = new FormData();
         bodyFormData.append("code", code);
         bodyFormData.append("state", input);
         bodyFormData.append("problemID", problem.id);
-        axios.post("/step", bodyFormData).then((res) => {
-            setInput(res.data.nextStatus);
-            if (res.data.isEnded == true) {
-                setOpenModal(true);
-                /**いいかんじに あたまが動かない*/
-            }
-        });
-    }
+        const res = await axios.post("/step", bodyFormData);
+        setInput(res.data.output);
+    };
 
     const excute = async () => {
         const bodyFormData = new FormData();
         bodyFormData.append("id", userId);
         bodyFormData.append("code", code);
-        bodyFormData.append("state", input);
+        bodyFormData.append("state", inputRef.current);
         bodyFormData.append("problemID", problem.id);
         const loop = async () => {
-            const step = await axios.post("/step", bodyFormData);
-            console.log(step);
-            if (step.data !== "") {
-                setTimeout(loop, 3000);
+            bodyFormData.set("state", inputRef.current);
+            const res = await axios.post("/step", bodyFormData);
+            console.log(res.data);
+            setInput(res.data.output);
+            bodyFormData.set("state", inputRef.current);
+            if (res.data.isEnded !== true) {
+                setTimeout(loop, 500);
             }
         };
         await loop();
@@ -88,7 +61,16 @@ const Problem = () => {
             <div className="my-8 flex justify-between justify-items-stretch">
                 <div className="justify-self-start basis-1/2 flex flex-wrap">
                     <Description title={problem.title} md={problem.text} />
-                    <Submission openModal={openModal} setOpenModal={setOpenModal} value={input} setValue={setInput} />
+                    <div className="bg-yellow-200 rounded m-4 p-4 basis-full flex flex-wrap">
+                        <h3 className="w-full my-3">提出 / 実行</h3>
+                        <input
+                            type="text"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            id="source"
+                            className="w-full h-8 my-3 font-mono"
+                        />
+                    </div>
                     <>
                         <button
                             className="bg-green-500 w-1/4 h-8 my-3 mr-3 rounded"
